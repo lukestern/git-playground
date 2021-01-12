64598a9deb7b11fe0ca123c41baa83fdd1b03a52
Luke Stern <Luke.Stern@softwire.com>
2021-01-12 14:15:05 +0000

    Workshop1 Ex1 pt1-2

---
+++ calc_part2.py
@@ -0,0 +1,31 @@
+f = open('Workshop1_ex1_part2_input.txt', 'r')
+lines = f.read().splitlines()
+
+def CheckAndCalculate(input):
+    parameters = input.split(' ')
+    if parameters[0] == 'calc':
+        CheckCalcParameters(parameters[1:])
+        print(Calc(parameters[1], parameters[2], parameters[3]))
+
+def CheckCalcParameters(parameters):
+    if parameters[0] not in ['/', 'x', '+', '-']:
+         raise ValueError("First argument must be an operation:\n Divde: '/'\n Multiply: 'x'(or '*')\n Add:'+'\n Subtract '-'")
+
+def Calc(op, a, b):
+    a = int(a)
+    b = int(b)
+    if op == '/':
+        result = a / b
+        return result
+    elif op == 'x' or op == '*':
+        result = a * b
+        return result
+    elif op == '+':
+        result = a + b
+        return result
+    elif op == '-':
+        result = a - b
+        return result
+
+for line in lines:
+    CheckAndCalculate(line)
